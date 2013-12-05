5242dc396a11914a233d293f2300b0191960ce5c
Hubert Lee <hubert.k.lee@gmail.com>
2013-12-05 14:21:03 +0800

    add log messages for launch and cleanup

--- chrome-5dp/js/launch.js
+++ chrome-5dp/js/launch.js
@@ -1,4 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
+    console.log("Launching 5DPrint");
     chrome.app.window.create('index.html', {
         'bounds': {
             'width': 768,
@@ -11,6 +12,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
 
 var devIds = [];
 var cleanup = function() {
+    console.log("Main 5DPrint window closed, cleaning up");
     chrome.power.releaseKeepAwake();
     for(var i = 0; i < devIds.length; i++) {
         chrome.serial.flush(devIds[i], function(info) { });
