a865c9726f3ba5432e49ba454781114338beb5c0
Hubert Lee <hubert.k.lee@gmail.com>
2013-12-06 15:45:31 +0800

    add acceleration M204

--- chrome-5dp/js/interface.js
+++ chrome-5dp/js/interface.js
@@ -229,7 +229,8 @@ function DeviceConfiguration() {
     this.fields = [
         ['steps', 'M92', ['x', 'y', 'z', 'e'], /M92 X(\d+) Y(\d+) Z(\d+) E(\d+)/],
         ['feedrate', 'M202', ['x', 'y', 'z', 'e'], /M202 X(\d+\.\d+) Y(\d+\.\d+) Z(\d+\.\d+) E(\d+\.\d+)/],
-        ['max-acceleration', 'M201', ['x', 'y', 'z', 'e'], /M201 X(\d+) Y(\d+) Z(\d+) E(\d+)/]
+        ['max-acceleration', 'M201', ['x', 'y', 'z', 'e'], /M201 X(\d+) Y(\d+) Z(\d+) E(\d+)/],
+        ['acceleration', 'M204', ['s', 't'], /M204 S(\d+\.\d+) T(\d+\.\d+)/]
     ];
     this.settings = this.fields.map(function(attrs) {
         return new Setting(attrs[0], attrs[1], attrs[2], attrs[3]);
